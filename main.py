c2ddb0b2c62beb03f29afaa1f79d58e2ea44e3a8
saeed114114 <rashedi_110@yahoo.com>
2025-09-22 22:38:04 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
 import requests
 import time
-import os
 import threading
 from flask import Flask
 
-# متغیرهای محیطی
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+# ⚡ توکن ربات را اینجا قرار بده
+BOT_TOKEN = "CCDBE0LFKZCPFRIVTEQMMPAQHFIZANGDNQYLWSBEFXXGXOGGOAYBTNPGUQJRWRWV"
 API_URL = f'https://botapi.rubika.ir/v3/{BOT_TOKEN}'
 
-app = Flask(__name__)  # اصلاح شده: name به جای name
+app = Flask(__name__)  # name صحیح است
 
 def get_updates(offset=None):
     url = f'{API_URL}/getUpdates'
@@ -47,6 +46,6 @@ if __name__ == "__main__":
     # اجرای ربات در یک Thread جدا
     threading.Thread(target=run_bot, daemon=True).start()
 
-    # دریافت پورت از متغیر محیطی لیارا
-    port = int(os.environ.get("PORT", 5000))
+    # پورت لیارا
+    port = 80
     app.run(host="0.0.0.0", port=port)
