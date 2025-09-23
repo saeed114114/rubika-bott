fc6f22d539bc184e2af748073db54e38cbd709a0
saeed114114 <rashedi_110@yahoo.com>
2025-09-23 00:43:24 -0700

    Add files via upload

--- set_webhook.py
+++ set_webhook.py
@@ -3,9 +3,8 @@ import requests
 BOT_TOKEN = "CCDBE0LFKZCPFRIVTEQMMPAQHFIZANGDNQYLWSBEFXXGXOGGOAYBTNPGUQJRWRWV"
 WEBHOOK_URL = "https://epic-dirac-epmedhnbm.liara.run/webhook"
 
-response = requests.post(
-    f"https://botapi.rubika.ir/v3/{BOT_TOKEN}/setWebhook",
-    json={"url": WEBHOOK_URL}
-)
+# آدرس صحیح
+WEBHOOK_API = f"https://botapi.rubika.ir/bot{BOT_TOKEN}/setWebhook"
 
+response = requests.post(WEBHOOK_API, json={"url": WEBHOOK_URL})
 print(response.json())
